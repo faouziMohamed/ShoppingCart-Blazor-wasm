4deee15347bd579a34827e6c0d99e1f6360ddbd5
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-09-21 03:33:44 +0100

    refactor(model): rename navigation property

---
+++ ShopOnline.Api/Migrations/20220921021901_UpdateForeignKeys.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopOnline.Api.Migrations
+{
+    public partial class UpdateForeignKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
